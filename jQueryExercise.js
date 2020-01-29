59e450b39b87ce43a1a68a4fcd2691538f434629
JamesJamesJamesJamesJames <James_Moon@lcca.com>
2020-01-29 16:54:54 -0500

    more jquery

--- jQueryExercise.js
+++ jQueryExercise.js
@@ -5,3 +5,14 @@ $("div.highlight").css("width", "200px");
 $("#third").css("border", "4px solid orange");
 
 $("div:first").css("color", "pink");
+
+// More jQuery Practice
+var styles = {
+    border: "orange dashed 10px",
+    background: "teal",
+    margin: 0,
+}
+
+$("h1").css(styles);
+
+$("div").css(styles);
